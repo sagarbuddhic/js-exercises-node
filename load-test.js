c12133e7075bf91bcb1590e7136635c7aa80cb18
SAGAR BUDDHI <sagar.buddhic@gmail.com>
2021-07-07 17:24:02 +1000

    Update load-test.js

--- load-test.js
+++ load-test.js
@@ -5,9 +5,13 @@ var axios = require('axios');
 
 var requestsCount = 0;
 var promises = [];
+var startDate = new Date(Date.now());
+var startMinutes = startDate.getMinutes();
+var startSeconds = startDate.getSeconds();
+console.log(`Start time - ${startDate.getHours()}:${startMinutes}:${startSeconds}`);
 
 while(requestsCount <  totalHits) {
-    promises.push(axios.get(topicsUrl));
+    promises.push(axios.get(url));
     requestsCount = requestsCount + 1;
 }
 
@@ -16,10 +20,15 @@ Promise.allSettled(promises)
     var count = 1;
       results.forEach((result) => {
         if (result.status === 'rejected') {
-            console.log(`webpage url: ${url} - hit: ${count} - status: ${result.reason.response.status},  status text: ${result.reason.response.statusText}`);
+            console.log(`Webpage url: ${url} - hit: ${count} - status: ${result.reason.response.status},  status text: ${result.reason.response.statusText}`);
         } else {
-            console.log(`webpage url: ${url} - hit: ${count}, status:  ${result.value.status}`);
+            console.log(`Webpage url: ${url} - hit: ${count}, status:  ${result.value.status}`);
         }
         count = count + 1;
       });
+      var endDate = new Date(Date.now());
+      var endSeconds = endDate.getSeconds();
+      var endMinutes = endDate.getMinutes();
+      console.log(`End time - ${endDate.getHours()}:${endMinutes}:${endSeconds}`);
+      console.log(`Total seconds to hit -  ${(endDate.getTime() - startDate.getTime()) / 1000}`);
 })
