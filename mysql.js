1037c7db32c8277d8f19754319c17c31a7e5cd2c
sagarbuddhic <sagar.buddhic@gmail.com>
2022-06-16 11:35:11 +0530

    mysql

---
+++ mysql.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "FIRSTDATABASE",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("connected");
+  connection.query("select * from firsttable;", (err, result) => {
+    if (err) throw err;
+    console.log(`result: ${JSON.stringify(result)}`);
+  });
+});
