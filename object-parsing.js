cfcc6308184aaf982d3424b37801cb2fc970a421
Sagar Buddhi <sagar.buddhi@domain.com.au>
2022-07-22 15:49:16 +0530

    object parsing

---
+++ object-parsing.js
@@ -0,0 +1,17 @@
+const parseObject = (data, params) => {
+    let localData = data;
+    let localParams = params.split('.') || [];
+
+    localParams.every((param) => {
+        localData = localData[param];
+        return localData !== undefined;
+    });
+    return localData;
+}
+
+const obj = {'a': {'b': {'c': 'test'}}};
+
+const response = parseObject(obj, 'a.b.c');
+const response1 = parseObject(obj, 'a.b.c.d');
+console.log(response);
+console.log(response1);
