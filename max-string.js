6309b27bb63d7e0803650151b8a41dab57d110d7
Sagar Buddhi <sagar.buddhi@domain.com.au>
2022-08-05 16:52:25 +0530

    max

---
+++ max-string.js
@@ -0,0 +1,11 @@
+function maxString(arr) {
+  if (!arr || !arr.length) {
+    return;
+  }
+
+  return arr.reduce((prev, curr) => {
+    return curr.length > prev.length ? curr : prev;
+  });
+}
+
+console.log(maxString(['ddd', 'dddddd', 'dddd', 'dddd', 'dd']));
