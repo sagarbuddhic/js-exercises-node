d3d77b8fc7cf31d98cdbc2482a6bc2cebb6aedf1
Sagar Buddhi <sagar.buddhi@domain.com.au>
2022-08-14 13:33:14 +0530

    min swamps

---
+++ general/minimum-swaps.js
@@ -0,0 +1,33 @@
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+function minSwaps(input) {
+    let output = 0;
+    let len = input.length;
+    let sorted = input.slice();
+    let map = new Map();
+
+    sorted.sort();
+
+    for (let i = 0; i < len; i++)
+        map.set(input[i], i);
+
+    for (let i = 0; i < len; i++) {
+        if (input[i] != sorted[i]) {
+            output++;
+            let temp = input[i];
+            swap(input, i, map.get(sorted[i]));
+
+            map.set(temp, map.get(sorted[i]));
+            map.set(sorted[i], i);
+        }
+    }
+
+    return output;
+}
+
+let ans = minSwaps([2, 1, 4, 3]);
+console.log(ans);
