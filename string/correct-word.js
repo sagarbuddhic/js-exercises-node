74f5cd84b1fbcb62fe9b7d6e671e31be4f0126de
Sagar Buddhi <sagar.buddhi@domain.com.au>
2022-08-16 14:13:18 +0530

    string

--- string/correct-word.js
+++ string/correct-word.js
@@ -1,15 +1,15 @@
-function myFunction (str) {
-    if(!str) {
+function myFunction(str) {
+    if (!str) {
         return;
-     }
+    }
 
     const arr = [...str];
-    const correctedArray = arr.map(e => String.fromCharCode(e.charCodeAt()+1));
+    const correctedArray = arr.map(e => String.fromCharCode(e.charCodeAt() + 1));
     return correctedArray.join('').replace('{', 'a');
-     
+
     //  let aToZ = 'abcdefghijklmnopqrstuvwxyz';
     //  let letters = str.split('');
-     
+
     //  let output = letters.map((letter, index) => {
     //     let curr = aToZ.indexOf(letter);
     //     let ind = curr + 1;
