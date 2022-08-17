2f4519823f098194e13c2bafb1c5da009f7ff063
Sagar Buddhi <sagar.buddhi@domain.com.au>
2022-08-17 15:12:15 +0530

    num to words

--- general/num-to-words.js
+++ general/num-to-words.js
@@ -1,6 +1,6 @@
 function numToWords(input) {
     let single = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let double = ['', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let double = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let tens = ['', 'ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     let incremental = ['', 'hundred', 'thousand', 'million', 'billion'];
     let inputArr;
@@ -9,20 +9,27 @@ function numToWords(input) {
     }
     let multiArr = [];
     let childArr = [];
-    for (let i = inputArr.length - 1; i >= 0; i -= 3) {
-        let count = 0;
-        childArr = [];
-        while (count < 3) {
-            inputArr[i - count] && childArr.push(inputArr[i - count]);
-            count++;
-        }
-        childArr.length > 0 && multiArr.push(childArr);
+    while (inputArr.length > 0) {
+        childArr = inputArr.splice((inputArr.length > 2 ? (inputArr.length - 3) : 0), 3);
+        multiArr.push(childArr.reverse());
     }
+    console.log(multiArr);
+
+    // for (let i = inputArr.length - 1; i >= 0; i -= 3) {
+    //     let count = 0;
+    //     childArr = [];
+    //     while (count < 3) {
+    //         inputArr[i - count] && childArr.push(inputArr[i - count]);
+    //         count++;
+    //     }
+    //     childArr.length > 0 && multiArr.push(childArr);
+    // }
 
     let concat = multiArr.reduce((prev, curr, index) => {
         let hundred = curr[2] && `${single[curr[2]]} hundred and `;
-        let second = curr[1] && `${tens[curr[1]]} `;
-        let set = `${hundred || ''}${second || ''}${single[curr[0]]}`;
+        let second = curr[1] && (curr[1] == 1 ? `${double[curr[0]]} ` : `${tens[curr[1]]} `);
+        let first = curr[1] != 1 ? `${single[curr[0]]}` : '';
+        let set = `${hundred || ''}${second || ''}${first || ''}`;
         switch (index) {
             case 0:
                 break;
