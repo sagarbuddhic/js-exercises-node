5349cc7eb133189c9bab009bbe58b94a633d2640
sagarbuddhic <sagar.buddhic@gmail.com>
2024-09-19 19:03:23 +0530

    fix code

--- string/correct-word.js
+++ string/correct-word.js
@@ -1,27 +1,29 @@
-// every letter in a word needs to be changed to next letter to get the right word. 
+// every letter in a word needs to be changed to next letter to get the right word.
 // For example 'zms', next letters of z,m,s is a,n,t, So the correct word is 'ant'.
 
 function myFunction(str) {
-    if (!str) {
-        return;
-    }
+  if (!str) {
+    return;
+  }
 
-    const arr = [...str];
-    const correctedArray = arr.map(e => String.fromCharCode(e.charCodeAt() + 1));
-    return correctedArray.join('').replace('{', 'a');
+  const arr = [...str];
+  const correctedArray = arr.map((e) =>
+    String.fromCharCode(e.charCodeAt() + 1)
+  );
+  return correctedArray.join("").replace("{", "a");
 
-    //  let aToZ = 'abcdefghijklmnopqrstuvwxyz';
-    //  let letters = str.split('');
+  //  let aToZ = 'abcdefghijklmnopqrstuvwxyz';
+  //  let letters = str.split('');
 
-    //  let output = letters.map((letter, index) => {
-    //     let curr = aToZ.indexOf(letter);
-    //     let ind = curr + 1;
-    //     ind = ind == aToZ.length ? 0: ind;
-    //      return aToZ[ind];
-    //  });
-    //  return output.join('');
+  //  let output = letters.map((letter, index) => {
+  //     let curr = aToZ.indexOf(letter);
+  //     let ind = curr + 1;
+  //     ind = ind == aToZ.length ? 0: ind;
+  //      return aToZ[ind];
+  //  });
+  //  return output.join('');
 
-    return correctedArray;
+  return correctedArray;
 }
 
-console.log(myFunction('zms'));
+console.log(myFunction("zms"));
