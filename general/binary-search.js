aaa5a0c7f251fce437bf2b5830e2c3d83d0b1981
sagarbuddhic <sagar.buddhic@gmail.com>
2025-04-06 17:23:43 +0530

    binary

--- general/binary-search.js
+++ general/binary-search.js
@@ -1,14 +1,6 @@
-// Given an array of integers nums which is sorted in ascending order,
-//  and an integer target, write a function to search target in nums.
-// You must write an algorithm with O(log n) runtime complexity.
+// leetcode 704 - binary search
 
-// Input: nums = [-1,0,3,5,9,12], target = 9
-// Output: 4
-// Explanation: 9 exists in nums and its index is 4
-
-// [-1,0,3,5,9,12]
-
-doesExist = (arr, val) => {
+let doesExist = (arr, val) => {
   let left = 0;
   let right = arr.length - 1;
   let middle;
