7b656e7e571101cf272da1f9b69fbc0bc63978a8
sagarbuddhic <sagar.buddhic@gmail.com>
2025-04-10 17:55:24 +0530

    non rep

--- exercises/string/non-rep-substring.js
+++ exercises/string/non-rep-substring.js
@@ -1,25 +1,27 @@
-// maximum substring without duplicate characters in a string. 
+// maximum substring without duplicate characters in a string.
 // example 'abbbcabcdefef', max substring here is 'abcdef'
+// leetcode3
+
 function nonRepsubString(str) {
-    let currString = [];
-    let longest = '';
-    let maxLen = 0;
+  let currString = [];
+  let longest = "";
+  let maxLen = 0;
 
-    for (let i = 0; i < str.length; i++) {
-        const currIndex = currString.indexOf(str[i]);
+  for (let i = 0; i < str.length; i++) {
+    const currIndex = currString.indexOf(str[i]);
 
-        if (currIndex !== -1) {
-            currString.splice(0, currIndex + 1);
-        }
-
-        currString.push(str[i]);
-        if (currString.length > maxLen) {
-            longest = currString.join('');
-            maxLen = currString.length;
-        }
+    if (currIndex !== -1) {
+      currString.splice(0, currIndex + 1);
     }
-    return { word: longest, max: maxLen };
+
+    currString.push(str[i]);
+    if (currString.length > maxLen) {
+      longest = currString.join("");
+      maxLen = currString.length;
+    }
+  }
+  return { word: longest, max: maxLen };
 }
 
-let str = nonRepsubString('abbbcabcdefef');
+let str = nonRepsubString("abbbcabcdefef");
 console.log(str);
