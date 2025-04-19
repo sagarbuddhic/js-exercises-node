6bca83ac5cabd787d1840a480082371fe7be6562
sagarbuddhic <sagar.buddhic@gmail.com>
2025-04-19 14:45:31 +0530

    update asteroid

--- exercises/arrays/asteroid-destroy.js
+++ exercises/arrays/asteroid-destroy.js
@@ -1,5 +1,4 @@
-//You are given an integer mass, which represents the original mass of a planet.
-// You are further given an integer array asteroids, where asteroids[i] is the mass of the ith asteroid.
+// leetcode2126
 
 let asteroidsDestroyed = (mass, astroids) => {
   const sorted = astroids.sort((a, b) => a - b);
--- exercises/general/armstrong.js
+++ exercises/general/armstrong.js
@@ -1,14 +1,15 @@
 // An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself.
 // Example 153 = 1(cube) + 5(cube) + 3(cube)
-function isArmstrongNumber(input) {
-    let number = input + '';
-    let power = number.length;
-    let sum = 0;
 
-    for (let i = 0; i < input.length; i++) {
-        sum += Math.pow(parseInt(input[i]), power);
-    }
-    return sum == input;
+function isArmstrongNumber(input) {
+  let number = input + "";
+  let power = number.length;
+  let sum = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    sum += Math.pow(parseInt(input[i]), power);
+  }
+  return sum == input;
 }
 
-console.log(isArmstrongNumber('153'));
+console.log(isArmstrongNumber("153"));
