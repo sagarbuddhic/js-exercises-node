b4d8dc962baf703a01b3aaf3d9c7c5e32b5d6168
sagarbuddhic <sagar.buddhic@gmail.com>
2025-10-29 09:28:55 +0530

    update

--- exercises/arrays/best-time-stock-1.js
+++ exercises/arrays/best-time-stock-1.js
@@ -4,7 +4,7 @@
 var maxProfit = function (prices) {
   let totalProfit = 0;
 
-  for (let i = 0; i < prices.length; i++) {
+  for (let i = 1; i < prices.length; i++) {
     if (prices[i] > prices[i - 1]) {
       totalProfit += prices[i] - prices[i - 1];
     }
